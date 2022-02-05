cb80bfa6b59f41dafaa8af4856857e8c4a5d198c
Abbas Tinwala <79747159+AbbasTinwala092@users.noreply.github.com>
2022-02-05 23:06:47 +0530

    Update 1365_How_Many_No_Are_Smaller_Than_Current_No.cpp

--- 1365_How_Many_No_Are_Smaller_Than_Current_No.cpp
+++ 1365_How_Many_No_Are_Smaller_Than_Current_No.cpp
@@ -21,3 +21,35 @@ public:
         
     }
 };
+//The above code have T=O(n^2); S=O(1)
+
+
+/*
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        
+        vector<int> dup(nums);
+        vector<int> lesser;
+        int j=0;
+        int counter = 0;
+        
+        sort(dup.begin(), dup.end());
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            j=0;
+            counter = 0;
+            while(nums[i]>dup[j])
+            {
+                counter++;
+                j++;
+            }
+            lesser.push_back(counter);
+        }
+        return lesser;
+        
+    }
+};
+*/
+//The above code have T=O(nlog(n)) (avg)
