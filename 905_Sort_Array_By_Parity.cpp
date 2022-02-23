65b840254462a37f9bdc08724ec1ca973702a8ee
Abbas Tinwala <79747159+AbbasTinwala092@users.noreply.github.com>
2022-02-23 12:17:35 +0530

    Update 905_Sort_Array_By_Parity.cpp

--- 905_Sort_Array_By_Parity.cpp
+++ 905_Sort_Array_By_Parity.cpp
@@ -1,3 +1,5 @@
+
+//Using extra vector
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
@@ -21,3 +23,41 @@ public:
 
 //T = O(n)
 //S = O(n)
+
+//Without using extra vector
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+        
+        if(nums.size()<=1)
+            return nums;
+        
+        while(start<end)
+        {
+            if(nums[start]%2==1 && nums[end]%2==1)
+                end--;
+            
+            else if(nums[start]%2==1 && nums[end]%2==0)
+            {
+                swap(nums[start], nums[end]);
+                start++;
+                end--;
+            }
+            
+            else if(nums[start]%2==0 && nums[end]%2==1)
+            {
+                end--;
+                start++;
+            }
+            
+            else if(nums[start]%2==0 && nums[end]%2==0)
+                start++;
+        }
+        return nums;
+    }
+};
+
+//T = O(n)
